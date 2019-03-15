386ea328f456bd61d39d0482160530f68e7c0304
crunchysoul <zzzcca12@gmail.com>
2019-03-15 16:51:41 +1100

    setup simple rn project with rnn

---
+++ index.js
@@ -0,0 +1,13 @@
+import { Navigation } from 'react-native-navigation';
+import { registerScreens } from './scr/screens';
+
+registerScreens();
+Navigation.events().registerAppLaunchedListener(() => {
+  Navigation.setRoot({
+    root: {
+      component: {
+        name: 'FooScreen',
+      },
+    },
+  });
+});
---
+++ scr/screens.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Navigation } from 'react-native-navigation';
+
+import Initial from './Initial';
+import Foo from './Foo';
+import Bar from './Bar';
+import Baz from './Baz';
+
+export const registerScreens = () => {
+  Navigation.registerComponent(`InitialScreen`, () => Initial);
+  Navigation.registerComponent(`FooScreen`, () => Foo);
+  Navigation.registerComponent(`BarScreen`, () => Bar);
+  Navigation.registerComponent(`BazScreen`, () => Baz);
+};
