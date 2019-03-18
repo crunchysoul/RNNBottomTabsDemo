bebc063e360f866713f4baf535acb5ed8b79e9e8
crunchysoul <zzzcca12@gmail.com>
2019-03-18 21:24:43 +1100

    add Quuz

---
+++ scr/Quuz.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import {
+  StyleSheet,
+  View,
+  Text,
+  Button,
+  TouchableOpacity,
+  Platform,
+} from 'react-native';
+import { Navigation } from 'react-native-navigation';
+import { styles } from './styles';
+
+export default class Quuz extends React.Component {
+  componentDidMount() {
+    this.navigationEventListener = Navigation.events().bindComponent(this);
+  }
+
+  navigationButtonPressed = ({ buttonId }) => {
+    switch (buttonId) {
+      case 'BtnQuuzTopBarRight':
+        Navigation.dismissModal(this.props.componentId);
+        break;
+    }
+  };
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.h1}>
+          {this.constructor.name}
+          ::src/screens/
+          {this.constructor.name}
+          .js
+        </Text>
+      </View>
+    );
+  }
+}
